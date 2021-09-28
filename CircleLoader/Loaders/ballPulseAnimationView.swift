0604978a0194a71c1f7fccb1e89e33480004394d
developer <usmanasghar.p@gmail.com>
2021-09-28 14:37:29 +0500

    commit after completing ball pulsing animation loader

--- CircleLoader/Loaders/ballPulseAnimationView.swift
+++ CircleLoader/Loaders/ballPulseAnimationView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct ballPulseAnimationView: View {
+    
+    @State var size  = false
+    @State var size2  = false
+    @State var size3  = false
+    @State var delay  = 0.0
+
     var body: some View {
-        Text("Hello, World! by usman" )
+        HStack(){
+            Circle()
+                .fill(Color.black)
+                .frame(width: 30, height: 30, alignment: .center)
+                .scaleEffect(size ? 0.3:1)
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true))
+                .onAppear() {
+                    self.size.toggle()
+                }
+            Circle()
+                .fill(Color.black)
+                .frame(width: 30, height: 30, alignment: .center)
+                .scaleEffect(size2 ? 0.3:1)
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).delay(0.1))
+                .onAppear() {
+                    self.size2.toggle()
+                     
+                }
+
+            Circle()
+                .fill(Color.black)
+                .frame(width: 30, height: 30, alignment: .center)
+                .scaleEffect(size3 ? 0.3: 1)
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).delay(0.2))
+                .onAppear() {
+                    self.size3.toggle()
+                }
+        }
     }
 }
 
