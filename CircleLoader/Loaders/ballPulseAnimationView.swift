c457e2587e7fec74a9ad5c1c19938866f7e4496d
developer <usmanasghar.p@gmail.com>
2021-09-28 10:58:26 +0500

    commit after adding file of ballPulseAnimation view

---
+++ CircleLoader/Loaders/ballPulseAnimationView.swift
@@ -0,0 +1,20 @@
+//
+//  ballPulseAnimationView.swift
+//  CircleLoader
+//
+//  Created by Macbook Pro on 28/09/2021.
+//
+
+import SwiftUI
+
+struct ballPulseAnimationView: View {
+    var body: some View {
+        Text("Hello, World! by usman" )
+    }
+}
+
+struct ballPulseAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ballPulseAnimationView()
+    }
+}
